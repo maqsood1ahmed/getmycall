5d7f2b94eff3da82497719274ba51e791f985f41
maqsood1ahmed <maqsood@livepin.tv>
2020-04-20 18:03:59 +0500

    add main-app folder with basic design

---
+++ main-app/counter_dist/precache-manifest.3a535478bad23141dd1b7152e8bcb443.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "f03472fc8fa6676d09b7",
+    "url": "/static/css/react-counter.css"
+  },
+  {
+    "revision": "f03472fc8fa6676d09b7",
+    "url": "/static/js/react-counter.js"
+  },
+  {
+    "revision": "4f0c3c32f395d43ee36f384cfc972cd7",
+    "url": "/index.html"
+  }
+];
